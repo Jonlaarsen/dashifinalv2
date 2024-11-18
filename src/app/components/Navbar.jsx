8785ede35671cc9d0d21962064cfbedab0c26790
Jonlaarsen <jontoftdallarsen@icloud.com>
2024-11-18 21:48:05 +0900

    second

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Navbar = () => {
+
   return (
     <div className='flex fixed justify-between px-7 items-center top-0 left-0 h-[4rem] w-screen '>
       <div>
-        <h1 className='text-2xl md:text-5xl font-extrabold title'><a href='#top'>DASHI</a></h1>
+        <h1 className='text-4xl md:text-5xl font-extrabold title'><a href='#top'>DASHI</a></h1>
       </div>
       {/* <div className='flex font-semibold text-xl uppercase gap-2 md:gap-10'>
         <a href='https://weiq.app/dashi'>order</a>
