b0f98610fe14c97efe5a85c144c844e14aecc520
Jonlaarsen <jontoftdallarsen@icloud.com>
2024-11-18 21:58:12 +0900

    third

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 const Navbar = () => {
+  const [colorChange, setColorchange] = useState(false);
+
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 60) {
+      setColorchange(true);
+    } else {
+      setColorchange(false);
+    }
+  };
+  
+  window.addEventListener("scroll", changeNavbarColor);
 
   return (
     <div className='flex fixed justify-between px-7 items-center top-0 left-0 h-[4rem] w-screen '>
       <div>
-        <h1 className='text-4xl md:text-5xl font-extrabold title'><a href='#top'>DASHI</a></h1>
+        <h1  className= {colorChange ?'text-4xl md:text-5xl font-extrabold title opacity-40' : 'text-4xl md:text-5xl font-extrabold title'}><a href='#top'>DASHI</a></h1>
       </div>
       {/* <div className='flex font-semibold text-xl uppercase gap-2 md:gap-10'>
         <a href='https://weiq.app/dashi'>order</a>
--- src/app/components/Order.jsx
+++ src/app/components/Order.jsx
@@ -12,9 +12,9 @@ const Order = () => {
         <img className=' w-screen  border-t-4 border-b-4   border-[#E43D12]' src="https://scontent-ssn1-1.xx.fbcdn.net/v/t39.30808-6/325313122_490044659940146_7697449917841712903_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=4181mxHgUJ8Q7kNvgFsax4g&_nc_zt=23&_nc_ht=scontent-ssn1-1.xx&_nc_gid=A6pOzOsJSfNhUnj6vTXqynW&oh=00_AYAgXCRsCmtaeFb4jRgi71GgLY8rzLXySN13Xb-pCSULpQ&oe=6740BA07" alt="order" />
       </div>
       <div className='flex flex-col items-center py-10 justify-center w-screen gap-10 bg-[url("https://www.wallpapershop.com.au/assets/full/WP30062.jpg?20230915123918")]'>
-            <h1 className='text-xl text-center font-semibold uppercase'>order <span className='font-extrabold'>now</span> by pressing the ramen</h1>
+            <h1 className='text-3xl text-center font-bold uppercase title'>order <span className='font-extrabold'>now</span> by pressing the ramen</h1>
             <a href='https://weiq.app/dashi'>
-                <img src="https://cdn-icons-png.flaticon.com/512/3480/3480765.png" alt="ramen" className='w-[15rem] md:w-[20rem] p-4 bg-[#E43D12] rounded-[50%]' />   
+                <img src="https://cdn-icons-png.flaticon.com/512/3480/3480765.png" alt="ramen" className='w-[15rem] md:w-[20rem] p-4  bg-[#E43D12] rounded-[50%]' />   
             </a>
       </div>
     </div>
